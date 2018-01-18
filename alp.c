b81e05136fe574a682c9f07543c085e9c4309be7
tsukumar <30828604+tsukumar@users.noreply.github.com>
2018-01-18 16:56:10 +0530

    Create alp.c

---
+++ alp.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    char a;
+    printf("Enter a character: ");
+    scanf("%c",&a);
+
+    if( (a>='a' && a<='z') || (a>='A' && a<='Z'))
+        printf("%c is an alphabet.",a);
+    else
+        printf("%c is not an alphabet.",a);
+}
