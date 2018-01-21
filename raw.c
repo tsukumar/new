8252bad20059a6ea2f70a86a1692c7e1de9bfadb
tsukumar <30828604+tsukumar@users.noreply.github.com>
2018-01-21 09:54:24 +0530

    Create raw.c

---
+++ raw.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+ int d,o,result;
+printf("\nenter the num");
+scanf("%d",&d);
+printf("\nenter the power val");
+scanf("%d",&o);
+result=pow(d,o);
+printf("%d",result);
+}
