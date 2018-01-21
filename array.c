505ce15d8533dabf47cdfc9fe4a442bec0db569b
tsukumar <30828604+tsukumar@users.noreply.github.com>
2018-01-21 09:36:32 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main() {
+	int a,b,c[50],i;
+	int sum=0;
+	scanf("%d\t%d",&a,&b);
+    for(i=0;i<a;i++)
+    {
+    	scanf("%d",&j[i]);
+    }
+    for(i=0;i<b;i++)
+    {
+    	sum=sum+j[i];
+    }
+    printf("%d",sum);
+}
